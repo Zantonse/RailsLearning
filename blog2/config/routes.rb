57c09133f0b48a86a85a529d1edf8a80ffde3d98
Zantonse <craig.verzosa@gmail.com>
2014-01-07 17:58:17 -0500

    Ch.5 done

--- blog2/config/routes.rb
+++ blog2/config/routes.rb
@@ -1,4 +1,6 @@
 Blog2::Application.routes.draw do
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
