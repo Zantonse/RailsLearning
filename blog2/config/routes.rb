e5d6ea3eeede01a65ab164a7df88102bce40c2db
Zantonse <craig.verzosa@gmail.com>
2014-01-16 23:01:37 -0500

    Adding Sessions

--- blog2/config/routes.rb
+++ blog2/config/routes.rb
@@ -4,6 +4,10 @@ Blog2::Application.routes.draw do
     resources :comments
   end
   resources :users
+  resource :session, :only => [:new, :create, :destroy]
+  get '/login' => "sessions#new", :as => "login"
+  get '/logout' => "sessions#destroy", :as => "logout"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
