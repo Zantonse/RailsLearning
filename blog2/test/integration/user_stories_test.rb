7c2a0fe3eeef03219d39ec728d8c75ecc1076a57
Zantonse <craig.verzosa@gmail.com>
2014-01-27 02:57:34 -0500

    Login and logout testing

--- blog2/test/integration/user_stories_test.rb
+++ blog2/test/integration/user_stories_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should login user and redirect" do
+    get login_path
+
+    assert_response :success
+    assert_template 'new'
+
+    post session_path, :email => 'eugene@example.com', :password => 'secret'
+
+    assert_response :redirect
+    assert_redirected_to root_path
+
+    follow_redirect!
+
+    assert_response :success
+    assert_template 'index'
+    assert session[:user_id]
+  end
+
+  test "should logout user and redirect" do
+    get logout_path
+
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_nil session[:user]
+
+    follow_redirect!
+
+    assert_template 'index'
+  end
 end
